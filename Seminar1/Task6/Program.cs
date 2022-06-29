776a90dea327fc6730aaabc1fb54da9bc1658169
SvetlanaB <kapk2004@inbox.ru>
2022-06-29 11:56:12 +0300

    Seminar1_Task6

---
+++ Seminar1/Task6/Program.cs
@@ -0,0 +1,10 @@
+﻿// Task6
+int numberA;
+Console.Write("Введите число = ");
+numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA % 2 == 0 )
+    Console.Write("да, число четное.");
+else
+    Console.Write("нет, число нечетное.");
+
