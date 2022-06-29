bafb327acfd9740f26961cf5a149296bd38ec2ff
SvetlanaB <kapk2004@inbox.ru>
2022-06-29 11:35:16 +0300

    Seminar1_Task2

---
+++ Seminar1/Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Task2
+int numberA;
+Console.Write("Введите число А = ");
+numberA = Convert.ToInt32(Console.ReadLine());
+
+int numberB;
+Console.Write("Введите число В = ");
+numberB = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+int min = numberA;
+
+if(numberB >= max) max = numberB;
+Console.WriteLine("Максимальное значение = " + max);
+if (numberB<= min) min = numberB;
+Console.Write("Минимальное значение= " + min);
