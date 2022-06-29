fd7de7b4051b3f6e2d2373287360589c834dec3b
SvetlanaB <kapk2004@inbox.ru>
2022-06-29 12:04:17 +0300

    Seminar1_Task8

---
+++ Seminar1/Task8/Program.cs
@@ -0,0 +1,15 @@
+﻿// Task8
+int num , current;
+
+Console.Write("Введите число N = ");
+num = Convert.ToInt32(Console.ReadLine());
+
+current=1;
+
+while(current< num)
+{
+    current =(current+1);
+        if (current % 2 == 0)
+            Console.Write(current +" ");
+        else;
+}
