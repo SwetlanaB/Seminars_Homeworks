07578f3be74981bf2a3c406f5a748000994d969b
SvetlanaB <kapk2004@inbox.ru>
2022-07-01 21:24:04 +0300

    Task10F added

---
+++ Seminar2/Task10F/Program.cs
@@ -0,0 +1,15 @@
+﻿// Task10F
+
+int SecondNum (int num)
+{   
+    return ((num / 10) % 10);
+}
+
+int numberA;
+Console.Write("Введите число А = ");
+numberA = Convert.ToInt32(Console.ReadLine());
+
+
+int number = SecondNum (numberA);
+
+Console.WriteLine("Ваше число будет=" +  number);
