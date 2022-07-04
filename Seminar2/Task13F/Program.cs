38b82382c5ae8c8c9cc6be8ed8bcf2efa2f480e9
SvetlanaB <kapk2004@inbox.ru>
2022-07-04 12:48:03 +0300

    Task13F added

---
+++ Seminar2/Task13F/Program.cs
@@ -0,0 +1,28 @@
+﻿// Task13F
+
+
+void ThirdNumber()
+{
+int a = new Random().Next(1, 2147483647);
+Console.WriteLine("Вывести на печать случайное число=" + a);
+
+int result=0;
+
+while (a > 1000) a = a / 10;
+{
+   if (a>100) 
+   {
+      result = a % 10;
+  
+      Console.Write("ТРЕТЬЯ ЦИФРА  =" + result);
+   }
+    
+   else 
+   {
+      Console.Write("Число не содержит третью цифру");
+   }
+   
+}
+}   
+
+ThirdNumber();  
