1f80953fde042f1b46f18f0ed41620eaeed043d7
SvetlanaB <kapk2004@inbox.ru>
2022-07-08 19:52:44 +0300

    Seminar4 added

---
+++ Seminar4/Task27/Program.cs
@@ -0,0 +1,26 @@
+﻿// Task27:Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе(452 -> 11)
+
+int Summa (int x)
+{
+   int summ=0;
+
+ while (x>0) 
+ {
+    summ = summ + x %10;
+    x=x/10;
+       
+ }
+   
+   return summ; 
+} 
+
+
+int number=0;
+Console.WriteLine("Вводим любое число:");
+number= Convert.ToInt32(Console.ReadLine());
+
+int a=Summa(number);
+
+Console.WriteLine("Сумма равна "+ a);
+
+ 
