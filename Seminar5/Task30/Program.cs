7a03b997abc90eabb6e2420fe62bde1818bb9b31
SvetlanaB <kapk2004@inbox.ru>
2022-07-09 22:55:05 +0300

    Task30 modifed

--- Seminar5/Task30/Program.cs
+++ Seminar5/Task30/Program.cs
@@ -1,12 +1,13 @@
-// Task34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+﻿// Task34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 
 
-int[] CreateRandomArray( int size,int minValue, int maxValue)
+int[] CreateRandomArray( int size,int minValue,int maxValue)
 {
 int[] newArray= new int[size];
 
+
 for (int i=0; i<size; i++)
-    newArray [i]=new Random().Next(minValue,maxValue+1);
+    newArray [i]=new Random().Next(100,999);
 
 return newArray;
 }
@@ -37,9 +38,9 @@ Console.WriteLine("Задайте размер массива ");
 int size=Convert.ToInt32(Console.ReadLine());
 
 
-int[]array=CreateRandomArray(size,min,max);
+int[]array=CreateRandomArray(size,100,999);
 
 ShowArray(array);
 
-int sumOfArray=FindQuantity(array);
-Console.WriteLine("Количество четных случайных значений массива равно " +sumOfArray);
+int quantOfArray=FindQuantity(array);
+Console.WriteLine("Количество четных случайных значений массива равно " +quantOfArray);
