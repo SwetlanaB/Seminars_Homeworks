0e182398c389cbc23b1f8ee0902fc4eef4cbf759
SvetlanaB <kapk2004@inbox.ru>
2022-07-28 19:30:05 +0300

    Task 64-the last

---
+++ Seminar9/Task64/Program.cs
@@ -0,0 +1,17 @@
+﻿/*//  Задайте значение N. Напишите программу, которая найдет кол-во цифр в числе N рекурсивным методом.
+
+N = 4532 -> 4*/
+
+int AccountFingers(int n)
+{
+    if (n>9)
+    {
+        return 1 + AccountFingers(n=n/10);
+    }  
+    else return 1;
+
+}
+Console.WriteLine(AccountFingers(542981279));
+
+
+
